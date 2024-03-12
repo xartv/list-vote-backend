5bbc2ba03209b4702ae86a30dc287ddc63dc03cb
Artem Vasilev <xartvdev@gmail.com>
2024-03-13 00:13:52 +0400

    fix origin in cors

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.use(cookieParser());
   app.enableCors({
-    origin: ['http://localhost:3001'],
+    origin: ['http://localhost:3000'],
     credentials: true,
     exposedHeaders: 'set-cookie',
   });
