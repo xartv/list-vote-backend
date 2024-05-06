713eb842a3ea93fce668ad3ca38331cf7a8fa05d
Artem Vasilev <xartvdev@gmail.com>
2024-05-06 23:11:20 +0400

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { ListModule } from './list/list.module';
 import { UserListModule } from './user-list/user-list.module';
 import { ListItemModule } from './list-item/list-item.module';
 import { RatingMarkModule } from './rating-mark/rating-mark.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
@@ -19,5 +21,7 @@ import { RatingMarkModule } from './rating-mark/rating-mark.module';
     ListItemModule,
     RatingMarkModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
